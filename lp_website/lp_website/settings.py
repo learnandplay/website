045fe6dcced2950fdd19f79ea86fddf5e67e23ee
zanchi-r <romainzanchi30@gmail.com>
2014-09-17 11:14:22 +0200

    add mysql config

--- lp_website/lp_website/settings.py
+++ lp_website/lp_website/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'lp_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['LP_MYSQL_DBNAME'],
+        'USER': os.environ['LP_MYSQL_USER'],
+        'PASSWORD': os.environ['LP_MYSQL_PASSWORD'],
+        'HOST': os.environ['LP_MYSQL_HOST'],
+        'PORT': os.environ['LP_MYSQL_PORT'],
     }
 }
 
