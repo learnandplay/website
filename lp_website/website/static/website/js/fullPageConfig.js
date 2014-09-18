9973feaaa3721bb87b5abae06cd311ed30f3bc50
zanchi-r <romainzanchi30@gmail.com>
2014-09-18 13:21:22 +0200

    Website : Basic single page structure without content

---
+++ lp_website/website/static/website/js/fullPageConfig.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#section-container').fullpage({
+	'verticalCentered': false,
+	'css3': true,
+	'sectionsColor': ['#F0F2F4', '#66aaee', '#a9cb3f', '#eeaa37'],
+	'navigation': true,
+	'navigationPosition': 'right',
+	'navigationTooltips': ["Page d'accueil", "Le projet", "L'équipe", "Les partenaires"]
+    });
+});
