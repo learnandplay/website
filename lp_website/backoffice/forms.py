53be4817e97039fd03a6cdbf4ca55a84ce583087
zanchi-r <romainzanchi30@gmail.com>
2014-09-26 13:49:50 +0200

    modif server side form validation for registration

--- lp_website/backoffice/forms.py
+++ lp_website/backoffice/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from django.contrib.auth.models import User
 from django import forms
 
@@ -11,13 +12,21 @@ class UserRegistrationForm(forms.ModelForm):
         help_texts = {
             'username': '',
         }
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Confirmation du mot de passe", widget=forms.PasswordInput)
+    password = forms.CharField(label="Mot de passe",
+                               widget=forms.PasswordInput(render_value = True, attrs={'placeholder': 'Mot de passe contenant au moins 8 caractères'}))
+    password_confirm = forms.CharField(label="Confirmation du mot de passe", widget=forms.PasswordInput(render_value = True))
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) < 8:
+            raise forms.ValidationError("Erreur ! Le mot de passe doit contenir au moins 8 caractères")
+        return password
     def clean_password_confirm(self):
         password = self.cleaned_data.get('password')
         password_confirm = self.cleaned_data.get('password_confirm')
-        if password and password_confirm and password != password_confirm:
-                raise forms.ValidationError("Erreur ! La confirmation du mot de passe ne correspond pas")
+        if not password:
+            raise forms.ValidationError("")
+        if password != password_confirm:
+            raise forms.ValidationError("Erreur ! La confirmation du mot de passe ne correspond pas")
         return password_confirm
 
 class UserLoginForm(forms.Form):
