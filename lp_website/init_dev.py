bacf1326804ec26681148c9922d70cf4ab5f7bd3
zanchi-r <romainzanchi30@gmail.com>
2015-04-08 14:31:03 -0700

    add statistics in init dev script

--- lp_website/init_dev.py
+++ lp_website/init_dev.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from django.contrib.auth.models import User, Group
-from backoffice.models import LPUser, SchoolClass
+from backoffice.models import LPUser, SchoolClass, Subject, Exercise, Statistics
 
 execfile("./init.py")
 
@@ -221,3 +221,88 @@ lp_students[4].append(LPUser())
 lp_students[4][4].user = students[4][4]
 lp_students[4][4].save()
 lp_students[4][4].school_class.add(school_classes[4])
+
+##
+## Create subjects
+##
+
+subjects = []
+
+subjects.append(Subject(name='Mathématiques'))
+subjects[0].save()
+subjects.append(Subject(name='Français'))
+subjects[1].save()
+subjects.append(Subject(name='Histoire'))
+subjects[2].save()
+
+##
+## Create Exercises
+##
+
+exercises = []
+
+exercises.append([])
+exercises[0].append(Exercise(name='Additions simples', subject=subjects[0]))
+exercises[0][0].save()
+exercises[0].append(Exercise(name='Additions avancées', subject=subjects[0]))
+exercises[0][1].save()
+exercises[0].append(Exercise(name='Soustractions', subject=subjects[0]))
+exercises[0][2].save()
+exercises[0].append(Exercise(name='Géométrie', subject=subjects[0]))
+exercises[0][3].save()
+exercises[0].append(Exercise(name='Problèmes', subject=subjects[0]))
+exercises[0][4].save()
+
+exercises.append([])
+exercises[1].append(Exercise(name='Lecture', subject=subjects[0]))
+exercises[1][0].save()
+
+##
+## Create Statistics
+##
+
+statistics = []
+
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[0][0], data="{'time':'64',success:'7','failure':'3'}"))
+statistics[0].save()
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[0][0], data="{'time':'58',success:'10','failure':'0'}"))
+statistics[1].save()
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[1][0], data="{'time':'127',success:'4','failure':'11'}"))
+statistics[2].save()
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[0][4], data="{'time':'590',success:'1','failure':'1'}"))
+statistics[3].save()
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[0][3], data="{'time':'30',success:'1','failure':'2'}"))
+statistics[4].save()
+statistics.append(Statistics(user=lp_students[0][0], exercise=exercises[0][4], data="{'time':'612',success:'2','failure':'0'}"))
+statistics[5].save()
+
+statistics.append(Statistics(user=lp_students[1][1], exercise=exercises[0][0], data="{'time':'64',success:'7','failure':'3'}"))
+statistics[6].save()
+statistics.append(Statistics(user=lp_students[1][1], exercise=exercises[0][0], data="{'time':'58',success:'10','failure':'0'}"))
+statistics[7].save()
+statistics.append(Statistics(user=lp_students[1][1], exercise=exercises[1][0], data="{'time':'127',success:'4','failure':'11'}"))
+statistics[8].save()
+statistics.append(Statistics(user=lp_students[1][1], exercise=exercises[0][3], data="{'time':'30',success:'1','failure':'2'}"))
+statistics[9].save()
+statistics.append(Statistics(user=lp_students[1][1], exercise=exercises[0][4], data="{'time':'612',success:'2','failure':'0'}"))
+statistics[10].save()
+
+statistics.append(Statistics(user=lp_students[0][3], exercise=exercises[1][0], data="{'time':'98',success:'11','failure':'4'}"))
+statistics[11].save()
+statistics.append(Statistics(user=lp_students[0][3], exercise=exercises[0][4], data="{'time':'301',success:'2','failure':'0'}"))
+statistics[12].save()
+statistics.append(Statistics(user=lp_students[0][3], exercise=exercises[0][3], data="{'time':'34',success:'1','failure':'2'}"))
+statistics[13].save()
+
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'74',success:'10','failure':'0'}"))
+statistics[14].save()
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'71',success:'9','failure':'1'}"))
+statistics[15].save()
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'61',success:'10','failure':'0'}"))
+statistics[16].save()
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'58',success:'7','failure':'3'}"))
+statistics[17].save()
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'67',success:'10','failure':'0'}"))
+statistics[18].save()
+statistics.append(Statistics(user=lp_students[0][1], exercise=exercises[0][0], data="{'time':'67',success:'10','failure':'0'}"))
+statistics[19].save()
