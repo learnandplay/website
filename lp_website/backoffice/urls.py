51c0d0ed900150b060354067422a2df2c836a51f
wayath <anthony@a-lingua.fr>
2015-11-02 16:35:08 +0100

    add route for restapi

--- lp_website/backoffice/urls.py
+++ lp_website/backoffice/urls.py
@@ -3,6 +3,7 @@ from backoffice import views
 
 urlpatterns = patterns('',
     url(r'^api/', include('backoffice.api.urls'), name='api'),
+    url(r'^restapi/', include('backoffice.restapi.urls'), name='restapi'),
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
