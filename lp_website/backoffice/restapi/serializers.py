aa267bbf4e3ba15e455f4460e7cd29808320885a
wayath <anthony@a-lingua.fr>
2015-11-02 19:51:46 +0100

    add get exercise-config

--- lp_website/backoffice/restapi/serializers.py
+++ lp_website/backoffice/restapi/serializers.py
@@ -35,3 +35,43 @@ class SubjectConfigSerializer(serializers.ModelSerializer):
         class Meta:
                 model = SubjectConfig
                 fields = ('id', 'name', 'start_date', 'end_date', 'data')
+
+## Classe ExerciseConfigSerializer\n
+# Serialise une configuration d'exercice
+class ExerciseConfigSerializer(serializers.ModelSerializer):
+        ## url pour éditer la configuration
+        edit_url = serializers.SerializerMethodField('generate_edit_url')
+        ## url pour supprimer la configuration
+        delete_url = serializers.SerializerMethodField('generate_delete_url')
+        ## nom de l'exercice de base
+        base_name = serializers.SerializerMethodField('generate_base_name')
+        ## classe a laquelle s'applique la configuration
+        school_class = serializers.SerializerMethodField('generate_school_class')
+        
+        ## Génère l'url pour éditer la configuration
+        # @param exerciseConfig La configuration
+        # @returns l'url pour éditer la configuration
+        def generate_edit_url(self, exerciseConfig):
+                return reverse('backoffice:exercise_configuration', kwargs={'exercise_id': exerciseConfig.id})
+        
+        ## Génère l'url pour supprimer la configuration
+        # @param exerciseConfig La configuration
+        # @returns l'url pour supprimer la configuration
+        def generate_delete_url(self, exerciseConfig):
+                return reverse('backoffice:api_delete_exercise_configuration')
+
+        ## Génère le nom de l'exercice de base
+        # @param exerciseConfig La configuration
+        # @returns Le nom de l'exercice
+        def generate_base_name(self, exerciseConfig):
+                return exerciseConfig.exercise.subject.name + ' - ' + exerciseConfig.exercise.name
+
+        ## Génère le nom de la classe associée
+        # @param exerciseConfig La configuration
+        # @returns Le nom de la classe associée
+        def generate_school_class(self, exerciseConfig):
+                return exerciseConfig.school_class.name + ' - ' + exerciseConfig.school_class.school_name
+
+        class Meta:
+                model = ExerciseConfig
+                fields = ('id', 'name', 'start_date', 'end_date', 'data', 'edit_url', 'delete_url', 'base_name', 'school_class')
--- lp_website/backoffice/restapi/views.py
+++ lp_website/backoffice/restapi/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from backoffice.models import LPUser, SchoolClass, Statistics, Subject, Exercise, SubjectConfig, ExerciseConfig
-from backoffice.restapi.serializers import SchoolClassSerializer, LPUserSerializer, SubjectConfigSerializer
+from backoffice.restapi.serializers import SchoolClassSerializer, LPUserSerializer, SubjectConfigSerializer, ExerciseConfigSerializer
 
 class JSONResponse(HttpResponse):
     def __init__(self, data, **kwargs):
@@ -52,4 +52,19 @@ class GetSubjectConfig(APIView):
         except (SchoolClass.DoesNotExist, SubjectConfig.DoesNotExist, Subject.DoesNotExist):
             return HttpResponse(status=400)
         serializer = SubjectConfigSerializer(subject_config, many=True)
+        return JSONResponse(serializer.data)
+
+class GetExerciseConfig(APIView):
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (JSONWebTokenAuthentication, )
+
+    def get(self, request, class_id, ref):
+        try:
+            user = request.user.LPUser
+            my_school_class = user.school_class.get(id=class_id)
+            exercise_ref = Exercise.objects.filter(reference=ref)
+            exercise_config = ExerciseConfig.objects.filter(school_class=my_school_class, exercise=exercise_ref).order_by('-start_date')[:1]
+        except (SchoolClass.DoesNotExist, ExerciseConfig.DoesNotExist, Exercise.DoesNotExist):
+            return HttpResponse(status=400)
+        serializer = ExerciseConfigSerializer(exercise_config, many=True)
         return JSONResponse(serializer.data)
