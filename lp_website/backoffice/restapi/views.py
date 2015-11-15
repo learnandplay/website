65b1962dd69d4ea1d0f7612e51a379ab7e24aa94
zanchi-r <romainzanchi30@gmail.com>
2015-11-15 14:18:10 +0100

    add comments for apidoc

--- lp_website/backoffice/restapi/views.py
+++ lp_website/backoffice/restapi/views.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -9,6 +10,49 @@ from rest_framework.renderers import JSONRenderer
 from backoffice.models import LPUser, SchoolClass, Statistics, Subject, Exercise, SubjectConfig, ExerciseConfig
 from backoffice.restapi.serializers import SchoolClassSerializer, LPUserSerializer, SubjectConfigSerializer, ExerciseConfigSerializer
 
+"""
+@apiDefine teacher_required
+@apiHeader {String} Authorization Format : "Authorization: JWT <your_token>" : Authentification de la requête
+"""
+
+"""
+@apiDefine UNAUTHORIZED
+@apiError UNAUTHORIZED Le token n'est pas inclus dans le header ou est invalide
+
+@apiErrorExample Error-Response:
+    HTTP 401 UNAUTHORIZED
+    {
+        "detail": "Authentication credentials were not provided."
+    }
+"""
+
+"""
+@api {post} /api-token-auth/ Récupérer le token d'authentification
+@apiParam {String} username Nom d'utilisateur
+@apiParam {String} password Mot de passe
+@apiVersion 0.1.0
+@apiName api-token-auth
+@apiGroup Authentification
+
+@apiSuccess {String} token Token d'authentification
+
+@apiSuccessExample Success-Response:
+    HTTP 200 OK
+    {
+        "token": "a_token"
+    }
+
+@apiError BADREQUEST La paire nom d'utilisateur / mot de passe n'est pas valide
+
+@apiErrorExample Error-Response:
+    HTTP 400 BAD REQUEST
+    {
+        "non_field_errors": [
+            "Unable to login with provided credentials."
+            ]
+    }
+"""
+
 class JSONResponse(HttpResponse):
     def __init__(self, data, **kwargs):
         content = JSONRenderer().render(data)
@@ -18,7 +62,28 @@ class JSONResponse(HttpResponse):
 class GetClasses(APIView):
     permission_classes = (IsAuthenticated, )
     authentication_classes = (JSONWebTokenAuthentication, )
+    """
+    @api {get} /classes/ Récupérer les classes de l'utilisateur
+    @apiUse teacher_required
+    @apiVersion 0.1.0
+    @apiName GetClasses
+    @apiGroup Classes
 
+    @apiSuccess {Object[]} classes             Liste des classes
+    @apiSuccess {Number}   classes.id          ID de la classe
+    @apiSuccess {String}   classes.name        Nom de la classe
+    @apiSuccess {String}   classes.school_name Nom de l'établissement
+    @apiSuccess {String}   classes.ip          IP du serveur
+
+    @apiSuccessExample Success-Response:
+        HTTP/1.1 200 OK
+        [
+            {"id":1,"name":"CP 1","school_name":"Ecole Albert Camus","ip":"127.0.0.1"},
+            {"id":2,"name":"CP 2","school_name":"Ecole Albert Camus","ip":"188.165.200.111"}
+        ]
+
+    @apiUse UNAUTHORIZED
+    """
     def get(self, request):
         user = request.user.LPUser
         classes = user.school_class.all().order_by('name')
@@ -28,7 +93,28 @@ class GetClasses(APIView):
 class GetStudents(APIView):
     permission_classes = (IsAuthenticated, )
     authentication_classes = (JSONWebTokenAuthentication, )
+    """
+    @api {get} /students/:class_id/ Récupérer la liste des élèves d'une classe
+    @apiUse teacher_required
+    @apiVersion 0.1.0
+    @apiName GetStudents
+    @apiGroup Classes
 
+    @apiSuccess {Object[]} students         Liste des étudiants
+    @apiSuccess {Number}   students.id      ID de l'étudiant
+    @apiSuccess {String}   student.username Nom d'utilisateur'
+
+    @apiSuccessExample Success-Response:
+        HTTP/1.1 200 OK
+        [
+            {"id":11,"username":"Anthony.Payet"},
+            {"id":3,"username":"Benjamin.Boisset"},
+            ...
+            {"id":8,"username":"Romain.Brunet"}
+        ]
+
+    @apiUse UNAUTHORIZED
+    """
     def get(self, request, class_id):
         try:
             user = request.user.LPUser
@@ -42,7 +128,33 @@ class GetStudents(APIView):
 class GetSubjectConfig(APIView):
     permission_classes = (IsAuthenticated, )
     authentication_classes = (JSONWebTokenAuthentication, )
+    """
+    @api {get} /subject-config/:class_id/:reference/ Récupérer la configuration d'une matière
+    @apiUse teacher_required
+    @apiVersion 0.1.0
+    @apiName GetSubjectConfig
+    @apiGroup Configuration
 
+    @apiSuccess {Number}    id          ID de la configuration
+    @apiSuccess {String}    name        Nom de la configuration
+    @apiSuccess {Date}      start_date  Date de début de la configuration
+    @apiSuccess {Date}      end_date    Date de fin de la configuration
+    @apiSuccess {Object}    data        Json de configuration
+    @apiSuccess {String}    reference   Référence de la matière
+
+    @apiSuccessExample Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "id":1,
+            "name":"Anglais bloqué",
+            "start_date":null,
+            "end_date":null,
+            "data":"{\"accessible\": false, \"config_name\": \"Anglais bloqué\", \"school_class\": \"2\"}",
+            "reference":"en"
+        }
+
+    @apiUse UNAUTHORIZED
+    """
     def get(self, request, class_id, ref):
         try:
             user = request.user.LPUser
@@ -57,7 +169,33 @@ class GetSubjectConfig(APIView):
 class GetExerciseConfig(APIView):
     permission_classes = (IsAuthenticated, )
     authentication_classes = (JSONWebTokenAuthentication, )
+    """
+    @api {get} /exercise-config/:class_id/:reference/ Récupérer la configuration d'un exercice
+    @apiUse teacher_required
+    @apiVersion 0.1.0
+    @apiName GetExerciseConfig
+    @apiGroup Configuration
 
+    @apiSuccess {Number}    id          ID de la configuration
+    @apiSuccess {String}    name        Nom de la configuration
+    @apiSuccess {Date}      start_date  Date de début de la configuration
+    @apiSuccess {Date}      end_date    Date de fin de la configuration
+    @apiSuccess {Object}    data        Json de configuration
+    @apiSuccess {String}    reference   Référence de l'exercice'
+
+    @apiSuccessExample Success-Response:
+        HTTP/1.1 200 OK
+        {
+            "id":1,
+            "name":"Config anglais lecture bloqué",
+            "start_date":null,
+            "end_date":null,
+            "data":"{\"accessible\": false, \"config_name\": \"Config anglais lecture bloqué\", \"school_class\": \"2\"}",
+            "reference":"en-lecture"
+        }
+
+    @apiUse UNAUTHORIZED
+    """
     def get(self, request, class_id, ref):
         try:
             user = request.user.LPUser
